26261386ef50f4f879962b5e3fa475f8fc9300ed
andreasalvi99 <andrea.salvi.r@gmail.comm>
2025-12-19 14:01:06 +0100

    array origined

---
+++ 01-mails/mails.js
@@ -0,0 +1,20 @@
+// ? Prima di tutto creo un array contenente alcune mail random
+// ? Queste sono le mail degli invitati all'evento
+const mails = [
+  "vateyeiyoike-3792@yopmail.com",
+  "brukennavoija-9080@yopmail.com",
+  "quannowouyoummei-3299@yopmail.com",
+  "guffoimuwoilla-9729@yopmail.com",
+  "briquadetouma-5131@yopmail.com",
+  "leuttasofruxu-7488@yopmail.com",
+  "kicabrevauno-8082@yopmail.com",
+  "deifoubrecrefrou-3526@yopmail.com",
+];
+
+// ? Chiedo all'utente di inserire la sua mail
+const userMail = prompt("Inserisci il tuo indirizzo email...");
+
+// ? Effettuo un controllo per verificare che la mail inserita dall'utente corrisponda a una di quelle in elenco
+for (let i = 0; i < mails.length; i++) {
+  currentMail = mails[i];
+}
